cd262d4b42bd1b1f93855f94ae7494f28f583810
itzmojahide2 <itzmojahide@gmail.com>
2025-09-29 20:53:27 +0600

    update src/main/java/io/github/itzclient/mixin/EntityRendererMixin.java

--- src/main/java/io/github/itzclient/mixin/EntityRendererMixin.java
+++ src/main/java/io/github/itzclient/mixin/EntityRendererMixin.java
@@ -2,48 +2,88 @@ package io.github.itzclient.mixin;
 
 import io.github.itzclient.modules.hud.NametagHud;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(EntityRenderer.class)
 public abstract class EntityRendererMixin<T extends Entity> {
 
-    @ModifyArgs(
-        method = "renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", 
+    @Unique
+    private static final Identifier ITZ_TAG_ICON = Identifier.of("itzclient", "textures/gui/tag_icon.png");
+
+    @Inject(
+        method = "renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
         at = @At(
-            value = "INVOKE", 
+            value = "INVOKE",
             target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"
-        )
+        ),
+        cancellable = true,
+        locals = LocalCapture.CAPTURE-FAILEXCEPTION
     )
-    private void itzclient$modifyNametag(Args args, T entity) {
+    private void itzclient$renderCustomNametag(T entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci,
+                                               double d, int y, Matrix4f matrix4f) {
+        
         NametagHud nametagModule = NametagHud.getInstance();
-        if (nametagModule.enabled.get() && nametagModule.showCustomTag.get()) {
+        if (nametagModule.enabled.get()) {
+            TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
             
-            Text originalText = args.get(0);
-            
-            String tagString = nametagModule.customTagText.get() + " ";
+            MutableText fullText = Text.empty();
+            String tagString = nametagModule.customTagText.get();
             Text customTag = Text.literal(tagString).styled(style -> style.withColor(nametagModule.customTagColor.get().toInt()));
             
-            Text newText;
-            if (nametagModule.tagPosition.get() == NametagHud.TagPosition.LEFT) {
-                newText = customTag.copy().append(originalText);
+            int iconSize = 10;
+            int iconPadding = 2;
+            int totalIconWidth = iconSize + iconPadding;
+
+            Text nameAndTag;
+            if (nametagModule.showCustomTag.get()) {
+                if (nametagModule.tagPosition.get() == NametagHud.TagPosition.LEFT) {
+                    nameAndTag = customTag.copy().append(" ").append(text);
+                } else {
+                    nameAndTag = text.copy().append(" ").append(customTag);
+                }
             } else {
-                newText = originalText.copy().append(" ").append(customTag);
+                nameAndTag = text.copy();
             }
 
-            // Update the text being rendered
-            args.set(0, newText);
+            float textWidth = textRenderer.getWidth(nameAndTag);
+            float totalWidth = textWidth + (nametagModule.showCustomTag.get() ? totalIconWidth : 0);
+            float xOffset = -totalWidth / 2.0f;
+            
+            float iconX = xOffset;
+            if (nametagModule.tagPosition.get() == NametagHud.TagPosition.RIGHT) {
+                iconX = xOffset + textWidth + iconPadding;
+            }
+            
+            float textX = xOffset;
+            if (nametagModule.showCustomTag.get() && nametagModule.tagPosition.get() == NametagHud.TagPosition.LEFT) {
+                textX += totalIconWidth;
+            }
 
-            // Re-center the nametag by adjusting its x-position
-            float oldWidth = MinecraftClient.getInstance().textRenderer.getWidth(originalText);
-            float newWidth = MinecraftClient.getInstance().textRenderer.getWidth(newText);
-            float oldX = args.get(1);
-            args.set(1, oldX - (newWidth - oldWidth) / 2.0f);
+            if (nametagModule.showCustomTag.get()) {
+                GuiGraphics graphics = new GuiGraphics(MinecraftClient.getInstance(), vertexConsumers.getBuffer(RenderLayer.getText()));
+                graphics.setMatrices(matrices);
+                graphics.drawTexture(ITZ_TAG_ICON, (int)iconX, -1, 0, 0, iconSize, iconSize, iconSize, iconSize);
+            }
+            
+            textRenderer.draw(nameAndTag, textX, 0.0f, 0x20FFFFFF, false, matrix4f, vertexConsumers, TextRenderer.TextLayerType.SEE_THROUGH, 0, light);
+            textRenderer.draw(nameAndTag, textX, 0.0f, -1, false, matrix4f, vertexConsumers, TextRenderer.TextLayerType.NORMAL, 0, light);
+        
+            ci.cancel();
         }
     }
 }
