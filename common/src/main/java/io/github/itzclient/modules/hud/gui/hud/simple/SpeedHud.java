81c6f3c013a01f1f2c16fd1eaf6089b03e6a11df
itzmojahide2 <itzmojahide@gmail.com>
2025-09-29 18:51:57 +0600

    update common/src/main/java/io/github/itzclient/modules/hud/gui/hud/simple/SpeedHud.java

--- common/src/main/java/io/github/itzclient/modules/hud/gui/hud/simple/SpeedHud.java
+++ common/src/main/java/io/github/itzclient/modules/hud/gui/hud/simple/SpeedHud.java
@@ -1,1 +1,50 @@
+package io.github.itzclient.modules.hud.gui.hud.simple;
 
+import io.github.axolotlclient.AxolotlClientConfig.api.options.Option;
+import io.github.axolotlclient.AxolotlClientConfig.impl.options.BooleanOption;
+import io.github.itzclient.bridge.entity.AxoEntity;
+import io.github.itzclient.bridge.entity.AxoPlayer;
+import io.github.itzclient.bridge.math.Vec3;
+import io.github.itzclient.bridge.util.AxoIdentifier;
+import io.github.itzclient.modules.hud.gui.entry.SimpleTextHudEntry;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Objects;
+
+public class SpeedHud extends SimpleTextHudEntry {
+
+    public static final AxoIdentifier ID = AxoIdentifier.of("itzclient", "speedhud");
+    private static final NumberFormat FORMATTER = new DecimalFormat("#0.00");
+    private final BooleanOption horizontal = new BooleanOption("horizontal", true);
+    private String speedString = "0.00 BPS";
+    private int tickCounter = 0;
+
+    @Override
+    public AxoIdentifier getId() { return ID; }
+    @Override
+    public List<Option<?>> getConfigurationOptions() { List<Option<?>> o = super.getConfigurationOptions(); o.add(horizontal); return o; }
+    @Override
+    public boolean tickable() { return true; }
+
+    @Override
+    public void tick() {
+        tickCounter++;
+        if (tickCounter >= 4) {
+            tickCounter = 0;
+            AxoPlayer player = client.br$getPlayer();
+            if (player == null) { this.speedString = getPlaceholder(); return; }
+            AxoEntity entity = Objects.requireNonNullElse(player.br$getVehicle(), player);
+            Vec3 vec = entity.br$getVelocity();
+            if (horizontal.get() || (entity.br$isOnGround() && vec.y() < 0)) {
+                vec = vec.y(0);
+            }
+            this.speedString = FORMATTER.format(vec.len() * 20) + " BPS";
+        }
+    }
+
+    @Override
+    public String getValue() { return this.speedString; }
+    @Override
+    public String getPlaceholder() { return "4.35 BPS"; }
+}
