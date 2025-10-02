8b086fd9faaff1c3cd3cce15f00d8031a74f3544
itzmojahide2 <itzmojahide@gmail.com>
2025-10-02 13:48:57 +0600

    update common/src/main/java/io/github/itzclient/modules/hud/gui/hud/simple/DayCounterHud.java

--- common/src/main/java/io/github/itzclient/modules/hud/gui/hud/simple/DayCounterHud.java
+++ common/src/main/java/io/github/itzclient/modules/hud/gui/hud/simple/DayCounterHud.java
@@ -29,7 +29,6 @@ public class DayCounterHud extends SimpleTextHudEntry {
 
     public static final AxoIdentifier ID = AxoIdentifier.of("itzclient", "daycounterhud");
 
-    // --- Caching variables for optimization ---
     private String dayCountString = "0 Days";
     private int tickCounter = 0;
 
@@ -45,17 +44,13 @@ public class DayCounterHud extends SimpleTextHudEntry {
 
     @Override
     public void tick() {
-        // Update the day count once per second (every 20 ticks)
         tickCounter++;
         if (tickCounter >= 20) {
             tickCounter = 0;
-
             if (client.br$getWorld() == null) {
                 this.dayCountString = getPlaceholder();
                 return;
             }
-
-            // The time of day is measured in ticks. 24000 ticks = 1 full day.
             long dayCount = client.br$getWorld().br$getTimeOfDay() / 24000L;
             this.dayCountString = AxoI18n.translate("daycounterhud.days", dayCount);
         }
@@ -63,7 +58,6 @@ public class DayCounterHud extends SimpleTextHudEntry {
 
     @Override
     public String getValue() {
-        // Return the cached day count string every frame.
         return this.dayCountString;
     }
 
