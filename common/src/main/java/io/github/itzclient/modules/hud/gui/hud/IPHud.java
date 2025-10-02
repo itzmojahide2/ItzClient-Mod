43d20a8b5401adc4651ae881dc09c4b00f5d8221
itzmojahide2 <itzmojahide@gmail.com>
2025-10-02 13:20:13 +0600

    update common/src/main/java/io/github/itzclient/modules/hud/gui/hud/IPHud.java

--- common/src/main/java/io/github/itzclient/modules/hud/gui/hud/IPHud.java
+++ common/src/main/java/io/github/itzclient/modules/hud/gui/hud/IPHud.java
@@ -41,28 +41,22 @@ public class IPHud extends TextHudEntry implements DynamicallyPositionable {
 
     public static final AxoIdentifier ID = AxoIdentifier.of("itzclient", "iphud");
 
-    // --- Settings for this module ---
     private final BooleanOption showIcon = new BooleanOption("iphud.show_icon", false);
     private final IntegerOption heightOption = new IntegerOption("hud.height", 13, 9, 64);
     private final EnumOption<AnchorPoint> anchor = new EnumOption<>("anchorpoint", AnchorPoint.class, AnchorPoint.TOP_LEFT);
-
     private AxoSprite.Dynamic sprite;
     private String serverAddress = "Singleplayer";
     private int tickCounter = 0;
 
     public IPHud() {
         super(115, 13, true);
-
-        // Listen for game events to manage the server icon
         Events.DISCONNECT.register(() -> {
             if (sprite != null) {
                 sprite.close();
                 sprite = null;
             }
         });
-
         Events.CONNECTION_PLAY_READY.register(() -> {
-            // Reset the tick counter to update immediately on join
             tickCounter = 20;
         });
     }
@@ -88,11 +82,9 @@ public class IPHud extends TextHudEntry implements DynamicallyPositionable {
 
     @Override
     public void tick() {
-        // Update the server address once per second
         tickCounter++;
         if (tickCounter >= 20) {
             tickCounter = 0;
-
             if (client.br$isLocalServer() || client.br$getServerAddress() == null) {
                 serverAddress = "Singleplayer";
                 if (sprite != null) {
@@ -101,12 +93,10 @@ public class IPHud extends TextHudEntry implements DynamicallyPositionable {
                 }
             } else {
                 serverAddress = client.br$getServerAddress();
-                // If the icon is enabled but not loaded, load it
                 if (showIcon.get() && sprite == null) {
                     sprite = PlatformDispatch.ipHud$getServerIcon();
                 }
             }
-            // If the icon is disabled but is loaded, close it
             if (!showIcon.get() && sprite != null) {
                 sprite.close();
                 sprite = null;
@@ -119,12 +109,10 @@ public class IPHud extends TextHudEntry implements DynamicallyPositionable {
         if (getHeight() != hNew) {
             setHeight(hNew);
         }
-
         int requiredWidth = graphics.br$getFont().br$getWidth(serverAddress) + 4;
         if (showIcon.get() && sprite != null) {
-            requiredWidth += getHeight() + 1; // Add space for the icon
+            requiredWidth += getHeight() + 1;
         }
-
         if (getWidth() != requiredWidth) {
             setWidth(requiredWidth);
             onBoundsUpdate();
@@ -136,30 +124,24 @@ public class IPHud extends TextHudEntry implements DynamicallyPositionable {
         updateSize(graphics);
         DrawPosition pos = getPos();
         int textX = pos.x() + getWidth() / 2;
-
         if (showIcon.get() && sprite != null) {
             int imageSize = getHeight() - 2;
             textX += imageSize / 2;
             graphics.br$drawTexture(pos.x() + 1, pos.y() + 1, imageSize, imageSize, sprite);
         }
-
-        graphics.br$drawCenteredString(serverAddress, textX, pos.y() + getHeight() / 2 - client.br$getFont().br$getFontHeight() / 2, textColor.get().toInt(), shadow.get());
+        graphics.br$drawCenteredString(serverAddress, textX, pos.y() + getHeight() / 2 - client.br$getFont().br$getFontHeight() / 2, textColor.get().toInt(), true);
     }
 
     @Override
     public void renderPlaceholderComponent(AxoRenderContext context, float delta) {
-        // Use the cached server address for the placeholder
         updateSize(context);
         DrawPosition pos = getPos();
         int textX = pos.x() + getWidth() / 2;
-
         if (showIcon.get()) {
             int imageSize = getHeight() - 2;
             textX += imageSize / 2;
-            // Draw a placeholder box for the icon
             context.br$fillRect(pos.x() + 1, pos.y() + 1, imageSize, imageSize, 0x80FFFFFF);
         }
-        
         context.br$drawCenteredString(serverAddress, textX, pos.y() + getHeight() / 2 - client.br$getFont().br$getFontHeight() / 2, textColor.get().toInt(), shadow.get());
     }
 
