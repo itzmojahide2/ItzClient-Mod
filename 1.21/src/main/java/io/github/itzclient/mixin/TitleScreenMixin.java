2adda5c7c8bca4bf6c48ca6fcca01cdd92d3420a
itzmojahide2 <itzmojahide@gmail.com>
2025-10-06 11:43:45 +0600

    update code

--- 1.21/src/main/java/io/github/itzclient/mixin/TitleScreenMixin.java
+++ 1.21/src/main/java/io/github/itzclient/mixin/TitleScreenMixin.java
@@ -22,16 +22,24 @@
 package io.github.itzclient.mixin;
 
 import io.github.itzclient.ui.screens.ItzMainMenuScreen;
+import net.minecraft.client.MinecraftClient; // Import MinecraftClient
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow; // Import Shadow
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin extends Screen {
+
+    // --- START OF FIX ---
+    // Add this @Shadow annotation to guarantee access to the client field.
+    @Shadow
+    protected MinecraftClient client;
+    // --- END OF FIX ---
 
     protected TitleScreenMixin(Text title) {
         super(title);
@@ -40,6 +48,7 @@ public abstract class TitleScreenMixin extends Screen {
     @Inject(method = "init", at = @At("HEAD"), cancellable = true)
     private void itzclient$replaceMainMenu(CallbackInfo ci) {
         ci.cancel();
+        // This will now compile correctly.
         this.client.setScreen(new ItzMainMenuScreen());
     }
 }
