d801d34bf8d7f8152b2ff2b1b58a1a3f85aa9233
itzmojahide2 <itzmojahide@gmail.com>
2025-10-06 11:49:53 +0600

    update 1.21/src/main/java/io/github/itzclient/mixin/GameMenuScreenMixin.java

--- 1.21/src/main/java/io/github/itzclient/mixin/GameMenuScreenMixin.java
+++ 1.21/src/main/java/io/github/itzclient/mixin/GameMenuScreenMixin.java
@@ -22,18 +22,30 @@
 package io.github.itzclient.mixin;
 
 import io.github.itzclient.ui.screens.HudEditorScreen;
+import net.minecraft.client.MinecraftClient; // Import MinecraftClient
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.TexturedButtonWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow; // Import Shadow
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameMenuScreen.class)
 public abstract class GameMenuScreenMixin extends Screen {
+
+    // --- START OF FIX ---
+    // These @Shadow annotations link the fields to the ones in the parent Screen class.
+    @Shadow
+    public int width;
+    @Shadow
+    public int height;
+    @Shadow
+    protected MinecraftClient client;
+    // --- END OF FIX ---
 
     private static final Identifier MOD_MENU_ICON = Identifier.of("minecraft", "textures/gui/sprites/widget/server_list.png");
 
@@ -43,6 +55,7 @@ public abstract class GameMenuScreenMixin extends Screen {
 
     @Inject(method = "initWidgets", at = @At("RETURN"))
     private void addModMenuButton(CallbackInfo ci) {
+        // This code will now compile correctly because 'width', 'height', and 'client' are accessible.
         this.addDrawableChild(new TexturedButtonWidget(
             this.width / 2 + 104,
             this.height / 4 + 72 + -16,
