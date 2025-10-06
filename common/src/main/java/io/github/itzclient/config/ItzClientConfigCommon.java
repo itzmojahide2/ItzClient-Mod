3e288001d6eac275c7cf1e29d8979c0a0ec92971
itzmojahide2 <itzmojahide@gmail.com>
2025-10-06 12:08:49 +0600

    update common/src/main/java/io/github/itzclient/config/ItzClientConfigCommon.java

--- common/src/main/java/io/github/itzclient/config/ItzClientConfigCommon.java
+++ common/src/main/java/io/github/itzclient/config/ItzClientConfigCommon.java
@@ -21,107 +21,32 @@
  */
 package io.github.itzclient.config;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.texture.NativeImage;
-import io.github.itzclient.ItzClientCommon;
-import io.github.itzclient.AxolotlClientConfig.api.options.Option;
-import io.github.itzclient.AxolotlClientConfig.api.options.OptionCategory;
-import io.github.itzclient.AxolotlClientConfig.api.ui.ConfigUI;
-import io.github.itzclient.AxolotlClientConfig.api.util.Color;
-import io.github.itzclient.AxolotlClientConfig.impl.options.*;
-import io.github.itzclient.config.screen.CreditsScreen;
-import io.github.itzclient.config.screen.ProfilesScreen;
-import io.github.itzclient.mixin.OverlayTextureAccessor;
-import io.github.itzclient.util.keybinds.KeyBinds;
-import io.github.itzclient.util.options.ForceableBooleanOption;
-import io.github.itzclient.util.options.GenericOption;
+import io.github.axolotlclient.AxolotlClientConfig.api.options.OptionCategory;
+import io.github.axolotlclient.AxolotlClientConfig.impl.options.BooleanOption;
 import lombok.Getter;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBind;
-import net.minecraft.client.texture.NativeImageBackedTexture;
 
-import java.util.ArrayList;
-import java.util.List;
+/**
+ * This is the common, version-independent configuration class.
+ * It only contains settings that do not rely on any Minecraft code.
+ * All the rendering and game-specific options have been moved to ItzClientConfig.java in the 1.21 module.
+ */
+@Getter
+public abstract class ItzClientConfigCommon {
 
-public class ItzClientConfig extends ItzClientConfigCommon {
+    // This is the main container for all configuration options.
+    protected final OptionCategory config = OptionCategory.create("config");
 
-    // --- Options from original client, now part of ItzClient ---
-    public final BooleanOption showOwnNametag = new BooleanOption("showOwnNametag", false);
-    public final BooleanOption useShadows = new BooleanOption("useShadows", false);
-    public final BooleanOption nametagBackground = new BooleanOption("nametagBackground", true);
-    public final ForceableBooleanOption timeChangerEnabled = new ForceableBooleanOption("enabled", false);
-    public final IntegerOption customTime = new IntegerOption("time", 0, 0, 24000);
-    public final BooleanOption customSky = new BooleanOption("customSky", true);
-    public final BooleanOption dynamicFOV = new BooleanOption("dynamicFov", true);
-    public final ForceableBooleanOption fullBright = new ForceableBooleanOption("fullBright", false);
-    public final BooleanOption removeVignette = new BooleanOption("removeVignette", false);
-    public final ForceableBooleanOption lowFire = new ForceableBooleanOption("lowFire", false);
-    public final BooleanOption lowShield = new BooleanOption("lowShield", false);
-    public final ColorOption hitColor = new ColorOption("hitColor", new Color(255, 0, 0, 77), value -> {
-        try {
-            NativeImageBackedTexture texture = ((OverlayTextureAccessor) MinecraftClient.getInstance().gameRenderer.getOverlayTexture()).axolotlclient$getTexture();
-            NativeImage nativeImage = texture.getImage();
-            if (nativeImage != null) {
-                int color = 255 - value.getAlpha();
-                color = (color << 8) + value.getBlue();
-                color = (color << 8) + value.getGreen();
-                color = (color << 8) + value.getRed();
-                for (int i = 0; i < 8; ++i) {
-                    for (int j = 0; j < 8; ++j) {
-                        nativeImage.setPixelColor(j, i, color);
-                    }
-                }
-                RenderSystem.activeTexture(33985);
-                texture.bindTexture();
-                nativeImage.upload(0, 0, 0, 0, 0, nativeImage.getWidth(), nativeImage.getHeight(), false, true, false, false);
-                RenderSystem.activeTexture(33984);
-            }
-        } catch (Exception ignored) {}
-    });
-    public final BooleanOption minimalViewBob = new BooleanOption("minimalViewBob", false);
-    public final BooleanOption noHurtCam = new BooleanOption("noHurtCam", false);
-    public final BooleanOption flatItems = new BooleanOption("flatItems", false);
-    public final BooleanOption noRain = new BooleanOption("noRain", false);
+    // This is a truly common option, as it only affects the logger.
+    public final BooleanOption debugLogOutput = new BooleanOption("debugLogOutput", false);
 
-    // --- NEW UNIVERSAL HUD STYLING OPTIONS ---
-    public final OptionCategory hudSettings = OptionCategory.create("hudSettings");
-    public final IntegerOption hudCornerRadius = new IntegerOption("hudCornerRadius", 0, 0, 10);
-    public final IntegerOption hudBackgroundOpacity = new IntegerOption("hudBackgroundOpacity", 100, 0, 255);
-    // --- END ---
+    // This category is used for internal settings that the user doesn't see.
+    public final OptionCategory hidden = OptionCategory.create("hidden");
 
-    // --- Categories ---
-    public final OptionCategory general = OptionCategory.create("general");
-    public final OptionCategory rendering = OptionCategory.create("rendering");
-    public final OptionCategory outlines = OptionCategory.create("blockOutlines");
-    public final OptionCategory timeChanger = OptionCategory.create("timeChanger");
-
-    @Getter
-    private final List<Option<?>> options = new ArrayList<>();
-
-    public ItzClientConfig() {
-        // Add all categories to the main config
-        config.add(general);
-        config.add(rendering);
-        config.add(hudSettings); // Add the new HUD settings category
-        rendering.add(outlines);
-        rendering.add(timeChanger);
-
-        // Add options to their categories
-        general.add(new GenericOption("profiles.title", "profiles.configure", () -> MinecraftClient.getInstance().setScreen(new ProfilesScreen(MinecraftClient.getInstance().currentScreen))), false);
-        
-        hudSettings.add(hudCornerRadius);
-        hudSettings.add(hudBackgroundOpacity);
-
-        rendering.add(customSky, dynamicFOV, fullBright, removeVignette, lowFire, lowShield, hitColor, minimalViewBob, flatItems, noHurtCam);
-        
-        timeChanger.add(timeChangerEnabled);
-        timeChanger.add(customTime);
-
-        outlines.add(new BooleanOption("enabled", false));
-        outlines.add(new ColorOption("color", Color.parse("#DD000000")));
-        
-        // Register keybinds with the correct category
-        var toggleFullbright = new KeyBind("toggle_fullbright", -1, "category.itzclient");
-        KeyBinds.getInstance().registerWithSimpleAction(toggleFullbright, fullBright::toggle);
+    protected ItzClientConfigCommon() {
+        // These are required by the config system but are not visible in the UI.
+        hidden.add(new BooleanOption("x", false));
+        hidden.add(new BooleanOption("y", false));
+        config.add(hidden);
+        config.add(debugLogOutput);
     }
 }
